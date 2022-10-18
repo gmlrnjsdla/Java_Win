f237c90a9179ba39a773b4268da28299ad080c37
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-10-18 12:47:43 +0900

    랜덤색 키 이벤트 추가

--- 20221018/win_project/WinRandomColor.java
+++ 20221018/win_project/WinRandomColor.java
@@ -8,10 +8,14 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JTextField;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class WinRandomColor extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -54,6 +58,23 @@ public class WinRandomColor extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		textField = new JTextField();
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode()== KeyEvent.VK_ENTER) {
+					int red   = (int)(Math.random()*256);
+					int green = (int)(Math.random()*256);
+					int blue  = (int)(Math.random()*256);
+					
+					contentPane.setBackground(new Color(red, green, blue));
+					setTitle("RGB ("+red+","+green+","+blue+")");
+				}
+			}
+		});
+		contentPane.add(textField);
+		textField.setColumns(10);
 	}
 
 }
