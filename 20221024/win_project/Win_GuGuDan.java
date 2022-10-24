14ee571b5314d27a9d430b09fcd42a1ffe56bb49
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-10-24 17:24:26 +0900

    구구단 출력 // TextArea연습

--- 20221024/win_project/Win_GuGuDan.java
+++ 20221024/win_project/Win_GuGuDan.java
@@ -13,10 +13,13 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.JTextField;
+import java.awt.Color;
 
 public class Win_GuGuDan extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtILoveSchool;
 
 	/**
 	 * Launch the application.
@@ -65,10 +68,18 @@ public class Win_GuGuDan extends JFrame {
 						textArea.append(i+" x "+j+" = "+(i*j)+"\n");	
 					}
 				}
-				
+				txtILoveSchool.requestFocus();
+				txtILoveSchool.setSelectionStart(0 );
+				txtILoveSchool.setSelectionEnd(txtILoveSchool.getText().length());
 			}
 		});
 		panel.add(btnNewButton);
+		
+		txtILoveSchool = new JTextField();
+		txtILoveSchool.setText("I LOVE SCHOOL");
+		txtILoveSchool.setBackground(new Color(255, 255, 0));
+		contentPane.add(txtILoveSchool, BorderLayout.SOUTH);
+		txtILoveSchool.setColumns(10);
 		
 		
 		
