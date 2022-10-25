29133edf904c8b35232febda5311f0277b66dcad
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-10-25 14:28:14 +0900

    Main/Sub 메소드를 통해 텍스트 전송

---
+++ 20221025/win_project/Win_Sub.java
@@ -0,0 +1,57 @@
+package win_project;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Win_Sub extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField tf_msg;
+
+	
+	public String getMsg() {
+		
+		return tf_msg.getText();
+	}
+	/**
+	 * Create the dialog.
+	 */
+	public Win_Sub() {
+		setTitle("Sub");
+		setBounds(100, 100, 285, 117);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			tf_msg = new JTextField();
+			contentPanel.add(tf_msg);
+			tf_msg.setColumns(20);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+		}
+	}
+
+}
